48a98b0e063d84a1fed6c8ed193cadb66c46d778
applestock <swebproger@gmail.com>
2023-02-20 19:12:16 +0100

    seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+30.times do
+  Product.create(
+    name: Faker::Commerce.product_name,
+    description: Faker::Lorem.paragraph(sentence_count: 3),
+    price: Faker::Commerce.price,
+    image_url: "https://loremflickr.com/#{rand(150..200)}/#{rand(150..200)}/all"
+    )
+end
